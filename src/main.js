b9e705720d2f6d7eb17234abff6ac46a3d9b3258
BYRON PIBAQUE <60248668+byronpibaque@users.noreply.github.com>
2024-04-01 10:05:32 -0500

    cambios 15porciento url correcta...

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 Vue.config.productionTip = false
 
 /*--------------------REGISTER BOOTSTRAP---------------------------------*/
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
 // Importar los archivos CSS de Bootstrap y BootstrapVue (el orden es importante)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -19,15 +19,14 @@ Vue.use(IconsPlugin);
 /*-----------------------------------------------------------------------*/
 
 
-
-    axios.defaults.baseURL='https://sistemabk03-service-pr-26.onrender.com/api/'; //RUTA DE ACCESO AL BACK-TEST
-  // axios.defaults.baseURL='https://sistemabk03-service.onrender.com/api/'; //RUTA DE ACCESO AL BACK
- //axios.defaults.baseURL='http://localhost:20000/api/'; //RUTA DE ACCESO AL BACKEND
+//axios.defaults.baseURL = 'https://sistemabk03-service-pr-26.onrender.com/api/'; //RUTA DE ACCESO AL BACK-TEST
+  axios.defaults.baseURL='https://sistemabk03-service.onrender.com/api/'; //RUTA DE ACCESO AL BACK
+//axios.defaults.baseURL='http://localhost:20000/api/'; //RUTA DE ACCESO AL BACKEND
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
 
 
